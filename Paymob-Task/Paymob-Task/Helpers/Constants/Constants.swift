de1a4cf5878129a6cc03d24473a6c8b047555145
eahmed <eahmed@telgani.com>
2024-07-06 17:37:36 +0300

    Create Constants.swift

---
+++ Paymob-Task/Paymob-Task/Helpers/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Paymob-Task
+//
+//  Created by iOSAYed on 06/07/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseURL = "https://api.themoviedb.org/3/movie/"
+}
