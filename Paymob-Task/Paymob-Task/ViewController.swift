acf1063b6ed40069bdab05b72967d440913f7ab7
eahmed <eahmed@telgani.com>
2024-07-06 12:34:12 +0300

    initial commit
    create Xcode project for iOS Application

---
+++ Paymob-Task/Paymob-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Paymob-Task
+//
+//  Created by iOSAYed on 06/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Paymob-Task/Paymob-TaskUITests/Paymob_TaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Paymob_TaskUITestsLaunchTests.swift
+//  Paymob-TaskUITests
+//
+//  Created by iOSAYed on 06/07/2024.
+//
+
+import XCTest
+
+final class Paymob_TaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
